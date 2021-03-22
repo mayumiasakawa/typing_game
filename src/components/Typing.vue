d81b570320fb563cce281d90acde81f51cf2959d
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-03-22 22:56:45 +0900

    add to template

---
+++ src/components/Typing.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="container">
+  <div class="title">Typing Game</div>
+  <h1>Typing Game</h1>
+  <div class="marker"></div>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
