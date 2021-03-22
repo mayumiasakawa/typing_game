15e8e8a1cf54f9f6d15ac97c4f591d596cf8059d
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-03-22 23:18:47 +0900

    add to html/s

--- src/components/Typing.vue
+++ src/components/Typing.vue
@@ -1,13 +1,88 @@
 <template>
-<div class="container">
-  <div class="title">Typing Game</div>
-  <h1>Typing Game</h1>
-  <div class="marker"></div>
-</div>
+  <div class="container">
+    <div class="title">
+      <h1>Typing Game</h1>
+      <div class="marker"></div>
+    </div>
+    <button class="startButton mb-20">START</button>
+    <div class="question mb-20">apple</div>
+    <div class="clear">Clear!</div>
+    <div class="typeformwapper mb-20">
+      <input type="text" class="typeform">
+    </div>
+    <div class="gaugeWarapper mb-20">
+      <div class="gauge"></div>
+    </div>
+    <div>1/5</div>
+  </div>
 </template>
 
 <script>
 </script>
 
 <style>
+  .mb-20{
+    margin-bottom: 20px;
+  }
+
+.container{
+  width: 400px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.title{
+  position: relative;
+  font-size: 48px;
+}
+
+.marker{
+  width: 100%;
+  height: 35%;
+  background-color: #a2a2a270;
+  position: absolute;
+  bottom: 5%;
+  z-index: -1;
+}
+
+.startButton{
+  background-color: #333;
+  color: #fff;
+  padding: 4px 60px;
+  border: none;
+  outline: none;
+  border-radius: 8px;
+  cursor: pointer;
+}
+
+.startButton:hover{
+  opacity: 0.7;
+}
+
+.question{
+  color: gray;
+}
+
+.clear{
+  color: #03a9f4;
+}
+
+.typeform{
+  text-align: center;
+  outline: none;
+  border: none;
+}
+
+.typeformwapper{
+  border-bottom: 1px solid gray;
+}
+
+.gauge{
+  height: 12px;
+}
+
+.gaugeWarapper{
+  border: 1px solid;
+  height: 12px;
+}
 </style>
